00be3dceeb9242fea9175c08315df7846fa64094
Your Name <you@example.com>
2022-12-12 07:36:02 +0900

    fix tests

--- tests/bot_maker/test_step.py
+++ tests/bot_maker/test_step.py
@@ -5,18 +5,6 @@ from src.bot_maker import BotMaker
 from src.logger import create_logger
 from src.utils import create_ccxt_client
 
-get_account_response_ftx = {
-    'result': {
-        'collateral': 1
-    },
-}
-
-fetch_positions_response_ftx = [{
-    'symbol': 'BTC/USD:USD',
-    'side': 'long',
-    'contracts': 1,
-}]
-
 
 get_account_response_binance = {
     'totalMarginBalance': '10000.0',
@@ -43,27 +31,8 @@ fetch_order_book_response_binance = {
     ]
 }
 
-class TestBotMakerStep(TestCase):
-    # def test_step_ftx(self):
-    #     agent_base_url = os.getenv('ALPHASEA_AGENT_BASE_URL')
-    #     logger = create_logger('debug')
-    #
-    #     client = create_ccxt_client(exchange='ftx')
-    #     client.privateGetAccount = MagicMock(return_value=get_account_response_ftx)
-    #     client.fetch_positions = MagicMock(return_value=fetch_positions_response_ftx)
-    #     client.create_order = MagicMock()
-    #
-    #     bot = Bot(
-    #         client=client,
-    #         logger=logger,
-    #         leverage=1.0,
-    #         agent_base_url=agent_base_url
-    #     )
-    #
-    #     bot._step()
-    #
-    #     client.create_order.assert_called()
 
+class TestBotMakerStep(TestCase):
     @mock.patch('time.time', mock.MagicMock(return_value=pd.to_datetime('2020/01/01 3:00:00', utc=True).timestamp()))
     def test_step_binance(self):
         logger = create_logger('debug')
@@ -74,21 +43,33 @@ class TestBotMakerStep(TestCase):
         client.fetch_ticker = MagicMock(return_value=fetch_ticker_response_binance)
         client.fetch_order_book = MagicMock(return_value=fetch_order_book_response_binance)
         client.create_order = MagicMock()
-        client.cancel_all_orders = MagicMock()
+        client.set_leverage = MagicMock()
 
         alphapool_client = MagicMock()
-        alphapool_client.get_positions = MagicMock(return_value=pd.DataFrame([
-            {
-                'model_id': 'pf-portfolio1',
-                'timestamp': pd.to_datetime('2020/01/01 00:00:00', utc=True),
-                'w.model1': 1.0,
-            },
+
+        t = pd.to_datetime('2020/01/01 00:00:00', utc=True).timestamp()
+        df = pd.DataFrame([
             {
                 'model_id': 'model1',
-                'timestamp': pd.to_datetime('2020/01/01 00:00:00', utc=True),
-                'p.BTC': 2.0,
-            },
-        ]).set_index(['model_id', 'timestamp']))
+                'timestamp': t,
+                'positions': {
+                    'BTC': 2.0
+                },
+                'weights': {},
+                'orders': {}
+            }, {
+                'model_id': 'pf-portfolio1',
+                'timestamp': t,
+                'positions': {},
+                'weights': {
+                    'model1': 1.0
+                },
+                'orders': {}
+            }
+        ])
+        df['timestamp'] = pd.to_datetime(df["timestamp"], utc=True, unit='s')
+        df = df.set_index(['timestamp', 'model_id']).sort_index()
+        alphapool_client.get_positions = MagicMock(return_value=df)
 
         bot = BotMaker(
             client=client,
@@ -106,6 +87,6 @@ class TestBotMakerStep(TestCase):
             'buy',
             1.0,
             9000.0,
-            { 'timeInForce': 'GTX' }
+            { 'timeInForce': 'GTX', 'reduceOnly': 'false' }
         )
-        client.cancel_all_orders.assert_called_with(symbol='BTC/USDT')
+        client.set_leverage.assert_called_with(10, 'BTC/USDT')
